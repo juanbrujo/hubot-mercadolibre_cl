99ee9bf315c6a1e593a67b8fbc0f1b1541003f4f
juanbrujo <juanbrujo@gmail.com>
2017-01-17 17:12:05 -0300

    init v0.0.1

---
+++ README.md
@@ -0,0 +1,28 @@
+# Hubot MercadoLibre Chile
+
+**Get available products from MercadoLibre.cl 🇨🇱**
+
+### Install:
+
+````
+$ npm install hubot-mercadolibre_cl --save
+````
+
+Add in `external-scripts.json`:
+
+````
+[
+  ..
+  "hubot-mercadolibre_cl"
+]
+````
+
+### Use:
+
+````
+hubot> hubot mercadolibre|meli|ml [product name]
+````
+
+### License:
+[MIT](https://opensource.org/licenses/MIT)
+
---
+++ mercadolibre.js
@@ -0,0 +1,72 @@
+// Description:
+//   Get available products from MercadoLibre.cl
+//
+//  Dependencies:
+//    cheerio
+//
+// Commands:
+//   hubot mercadolibre|meli|ml [product name]
+//
+// Author:
+//   @juanbrujo
+
+var cheerio = require('cheerio');
+
+module.exports = function(robot) {
+  robot.respond(/(mercadolibre|meli|ml) (.*)/i, function(msg) {
+    msg.send(':mercado: buscando...');
+
+    var search = msg.match[2];
+    var mainUrl = 'http://listado.mercadolibre.cl/';
+    var url = mainUrl + search.replace(' ', '-');
+
+    msg.robot.http(url).get()(function(err, res, body){
+      var $ = cheerio.load(body);
+      var resultados = [];
+      var errorText = $('main .nav-main-content .ml-main .ch-box.zrp-box .typo');
+
+      if( errorText.length ) {
+
+        var notFound = errorText.text().trim();
+        msg.send('No hay publicaciones que coincidan con tu búsqueda.');
+
+      } else {
+
+        $('#searchResults .results-item').each(function(){
+
+          var title = $(this).find('.list-view-item-title a').text();
+          var link = $(this).find('.list-view-item-title a').attr('href');
+          var price = $(this).find('.price-info .price-info-cost .ch-price').text();
+          resultados.push( '<' + link + '|' + title + ': ' + price + '>' );
+
+       });
+
+        if (resultados.length > 0) {
+
+          var limiteResultados = (resultados.length > 6) ? 5 : resultados.length;
+          var plural = resultados.length > 1 ? ['n','s'] : ['',''];
+          var text = 'Se ha'+plural[0]+' encontrado '+ resultados.length + ' resultado'+plural[1] + '\n';
+          
+          for (var i=0; i < limiteResultados; i++) {
+            var conteo = i + 1;
+            text += conteo + ': ' + resultados[i] + '\n';
+          }
+
+          if(resultados.length > limiteResultados) {
+            text += 'Otros resultados en: *<'+ url + '|MercadoLibre Chile>*\n';
+          }
+
+          if (robot.adapter.constructor.name === 'SlackBot') {
+            var options = {unfurl_links: false, as_user: true};
+            robot.adapter.client.web.chat.postMessage(msg.message.room, text, options);
+          } else {
+            msg.send(text);
+          }
+
+        }
+      }
+
+
+    });
+  });
+};
